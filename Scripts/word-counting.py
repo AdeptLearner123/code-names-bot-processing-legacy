4ab1ec7f049fc077e02ed842d425cc26d66ff5c2
Nalu Zou <zonalu@amazon.com>
2021-07-24 12:19:27 -0700

    First Commit

---
+++ Scripts/word-counting.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from nltk.stem.porter import *
+
+url_prefix = 'https://en.wikipedia.org/wiki/'
+
+def count(title, words):
+    counts = {}
+    for word in words:
+        counts[word] = 0
+        
+    stemmer = PorterStemmer()
+    reqs = requests.get(url_prefix + title)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+    body = soup.find('div', {'id': 'bodyContent'})
+    text = body.text.lower()
+    text = re.sub("[^a-zA-Z']+", " ", text)
+    
+    for word in text.split(' '):
+        word = stemmer.stem(word)
+        if word in words:
+            counts[word] += 1
+            
+    return counts
+
+print(count('Khidr', ['green', 'angel']))
