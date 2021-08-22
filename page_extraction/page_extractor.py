8d8174d584be63bce470f974ea896632e92de0e8
Nalu Zou <zonalu@amazon.com>
2021-08-21 20:39:20 -0700

    Count nouns only, count each proper noun separately

--- page_extraction/page_extractor.py
+++ page_extraction/page_extractor.py
@@ -1,16 +1,13 @@
-import re
-from nltk.stem.porter import *
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
-from urllib.request import urlopen
-from urllib import parse
-import json
 from bs4 import BeautifulSoup
 import unidecode
-from socket import timeout
-import sys
+import re
+from nltk import word_tokenize
+from nltk import sent_tokenize
+from nltk import pos_tag
+from nltk.stem import WordNetLemmatizer
 
-URL_PREFIX = 'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles='
+from utils.nlp_utils import get_ne_chunks
+
 
 def count_terms(page_title, target_term, text):
     term_counts, excerpts = count_terms_multi(page_title, [target_term], text)
@@ -18,116 +15,17 @@ def count_terms(page_title, target_term, text):
 
 
 def count_terms_multi(page_title, target_terms, text):
-    # Returns a map of counts for each target term.
-    # If a term has n words, gives 1/n for each occurrence of the word.
-    
-    #text = download_text(page_title)
-    #if text is None:
-    #    return None, None
     sentences = get_sentences(text)
-    term_words = get_term_words(target_terms)
-    title_words = get_term_words([page_title])[page_title]
-    sentence_counts, sentence_title_counts = count_words(term_words, title_words, sentences)
-    term_counts = agg_term_counts(sentence_counts, term_words)
-    excerpts = extract_excerpt(sentences, sentence_counts, sentence_title_counts, term_words)
-    return term_counts, excerpts
+    sentence_nouns_list, sentence_ne_list = get_sentence_nouns(sentences)
 
-
-def agg_term_counts(sentence_counts, term_words):
+    _, _, title_sentence_counts = count_term(page_title, sentences, sentence_nouns_list, sentence_ne_list)
     term_counts = {}
-    for term in term_words:
-        term_counts[term] = 0
-    for sentence_count in sentence_counts:
-        for term in sentence_count:
-            term_counts[term] += sentence_count[term]
-    return term_counts
-
-
-def count_words(term_words, title_words, sentences):
-    stemmer = PorterStemmer()
-    lemmatizer = WordNetLemmatizer()
-    stop_words = set(stopwords.words('english'))
-    sentence_counts = []
-    sentence_title_counts = []
-    for sentence in sentences:
-        term_counts, title_count = count_sentence_terms(stemmer, lemmatizer, stop_words, sentence, term_words, title_words)
-        sentence_counts.append(term_counts)
-        sentence_title_counts.append(title_count)
-    return sentence_counts, sentence_title_counts
-
-
-def count_sentence_terms(stemmer, lemmatizer, stop_words, sentence, term_words, title_words):
-    sentence = re.sub("[^a-zA-Z'.?!]+", " ", sentence)
-
-    word_counts = {}
-    for term in term_words:
-        for word in term_words[term]:
-            word_counts[word] = 0
-    
-    for word in title_words:
-        word_counts[word] = 0
-
-    sentence = sentence.lower()
-    sentence_words = sentence.split(' ')
-    for word in sentence_words:
-        if word in stop_words:
-            continue
-        if word in word_counts:
-            word_counts[word] += 1
-            continue
-        word_stemmed = stemmer.stem(word)
-        if word_stemmed in word_counts:
-            word_counts[word_stemmed] += 1
-            continue
-        word_lemmatized = lemmatizer.lemmatize(word)
-        if word_lemmatized in word_counts:
-            word_counts[word_lemmatized] += 1
-    
-    term_counts = {}
-    for term in term_words:
-        total = 0
-        for word in term_words[term]:
-            total += word_counts[word]
-        total /= len(term_words[term])
-        term_counts[term] = total
-    
-    title_total = 0
-    for word in title_words:
-        title_total += word_counts[word]
-    title_total /= len(title_words)
-
-    return term_counts, title_total
-
-
-def get_term_words(target_terms):
-    term_words = {}
+    term_excerpts = {}
     for term in target_terms:
-        formatted_term = term
-        if '_(' in formatted_term:
-            index = formatted_term.index('_(')
-            formatted_term = formatted_term[:index]
-        formatted_term = formatted_term.lower()
-        term_words[term] = []
-        for word in formatted_term.lower().replace('_', ' ').split(' '):
-            term_words[term].append(word)
-    return term_words
-
-
-def download_text(page_title):
-    formatted_title = page_title.replace('\\', '')
-    url = URL_PREFIX + parse.quote_plus(formatted_title)
-
-    try:
-        response = urlopen(url, timeout=10)
-        data_json = json.loads(response.read())
-        return list(data_json['query']['pages'].values())[0]['extract']
-    except timeout:
-        print('socket timed out - URL %s', url)
-        return None
-    except:
-        print("Failed to download text: " + page_title + " " + url)
-        print("Unexpected error:", sys.exc_info()[0])
-        return None
+        counted_term, count, sentence_counts = count_term(term, sentences, sentence_nouns_list, sentence_ne_list)
+        term_counts[term] = count
+        term_excerpts[term] = get_excerpt(sentence_counts, title_sentence_counts, sentences)
+    return term_counts, term_excerpts
 
 
 def get_sentences(html):
@@ -141,36 +39,84 @@ def get_sentences(html):
     text = re.sub(' +', ' ', text)
     text = text.replace(' ,', ',').replace(' .', '.')
 
-    text = text\
-        .replace('St. ', 'St.')\
-        .replace('Mr. ', 'Mr.')\
-        .replace('Ms. ', 'Ms.')\
-        .replace('Mrs. ', 'Mrs.')\
-        .replace('Dr. ', 'Dr.')\
-        .replace('Lt. ', 'Lt.')
     text = text.replace('\n', '. ')
-    sentences = text.split('. ')
+    sentences = sent_tokenize(text)
     sentences = list(map(lambda sentence:sentence.strip(), sentences))
     return sentences
 
 
-def extract_excerpt(sentences, sentence_counts,  sentence_title_counts, term_words):
-    term_excerpts = {}
-    for term in term_words:
-        term_excerpts[term] = extract_excerpt_term(sentences, sentence_counts, sentence_title_counts, term)
-    return term_excerpts
+def get_sentence_nouns(sentences):
+    lemmatizer = WordNetLemmatizer()
+    sentence_nouns_list = []
+    sentence_ne_list = []
 
+    for sentence in sentences:
+        pos_words = pos_tag(word_tokenize(sentence))
 
-def extract_excerpt_term(sentences, sentence_counts, clue_sentence_counts, term):
-    half_excerpts = []
-    for i in range(len(sentences)):
-        term_count = sentence_counts[i][term]
-        clue_count = clue_sentence_counts[i]
-        if term_count >= 1 and clue_count >= 1:
-            return sentences[i]
-        elif term_count >= 1:
-            half_excerpts.append(sentences[i])
+        sentence_nouns = filter(lambda word_pos: word_pos[1].startswith('NN') and not word_pos[1] == "NNP", pos_words)
+        sentence_nouns = list(map(lambda noun_pos:lemmatizer.lemmatize(noun_pos[0]), sentence_nouns))
+        sentence_nouns_list.append(sentence_nouns)
     
-    if len(half_excerpts) > 0:
-        return half_excerpts[0]
-    return ""
+        sentence_ne_list.append(get_ne_chunks(pos_words))
+
+    return sentence_nouns_list, sentence_ne_list
+
+
+def count_term(term, sentences, sentence_nouns_list, sentence_ne_list):
+    if ' ' in term:
+        return count_multi_word_term(term, sentences)
+    return count_single_word_term(term, sentence_nouns_list, sentence_ne_list)
+
+
+def count_single_word_term(term, sentence_nouns_list, sentence_ne_list):
+    common_noun_count = 0
+    common_noun_sentence_counts = []
+    for nouns_list in sentence_nouns_list:
+        count = len(list(filter(lambda noun:noun.lower() == term.lower(), nouns_list)))
+        common_noun_count += count
+        common_noun_sentence_counts.append(count)
+
+    ne_counts = {}
+    ne_sentence_counts = []
+    for ne_list in sentence_ne_list:
+        filtered_ne_list = list(filter(lambda ne:term.lower() in (word.lower() for word in word_tokenize(ne)), ne_list))
+        curr_ne_counts = {}
+        for ne in filtered_ne_list:
+            if ne in curr_ne_counts:
+                curr_ne_counts[ne] += 1
+            else:
+                curr_ne_counts[ne] = 1
+
+            if ne in ne_counts:
+                ne_counts[ne] += 1
+            else:
+                ne_counts[ne] = 1
+        ne_sentence_counts.append(curr_ne_counts)
+
+    max_ne = max(ne_counts, key=ne_counts.get) if len(ne_counts) > 0 else None
+
+    if max_ne is None or common_noun_count > ne_counts[max_ne]:
+        return term.lower(), common_noun_count, common_noun_sentence_counts
+    
+    max_ne_sentence_counts = list(map(lambda ne_count:ne_count[max_ne] if max_ne in ne_count else 0, ne_sentence_counts))
+    return max_ne, ne_counts[max_ne], max_ne_sentence_counts
+
+
+def count_multi_word_term(term, sentences):
+    total_count = 0
+    sentence_counts = []
+    for sentence in sentences:
+        count = sentence.lower().count(term.lower())
+        total_count += count
+        sentence_counts.append(count)
+    return term.lower(), total_count, sentence_counts
+
+
+def get_excerpt(term_sentence_counts, title_sentence_counts, sentences):
+    half_excerpt = None
+    for i in range(len(term_sentence_counts)):
+        if title_sentence_counts[i] > 0 and term_sentence_counts[i] > 0:
+            return sentences[i]
+        if term_sentence_counts[i] > 0 and half_excerpt is None:
+            half_excerpt = sentences[i]
+    return half_excerpt
