d61d17670f133909f4b3a3affe074fbf868d9429
Nalu Zou <zonalu@amazon.com>
2021-08-28 20:13:27 -0700

    Count nouns instead of noun chunks

--- page_extraction/source_page_extracts_job.py
+++ page_extraction/source_page_extracts_job.py
@@ -11,62 +11,26 @@ from page_downloads import page_downloader
 from page_downloads import page_downloads_database
 from nltk.stem import WordNetLemmatizer
 
-def get_source_page_counts(title, term):
+def get_source_page_counts(title):
     page_id = wiki_database.title_to_id(title)
     page_id = wiki_database.get_redirected_id(page_id)
     
-    text = page_extractor.format_text(page_downloads_database.get_content(title))
-
-    first_outgoing_ids = wiki_database.fetch_outgoing_links_set([page_id])
-    second_outgoing_ids = wiki_database.fetch_outgoing_links_set(first_outgoing_ids)
-    
-    first_words = page_ids_to_words(first_outgoing_ids)
-    second_words = page_ids_to_words(second_outgoing_ids)
-    words = first_words.union(second_words)
-
-    print("Words: {0} {1} {2}".format(len(first_words), len(second_words), len(words)))
-
-    root_counts, root_chunks = page_extractor.extract_noun_chunks(page_downloads_database.get_content(title))
-    word_counts = dict()
-    word_chunks = dict()
-    for word in words:
-        if word in root_counts:
-            word_counts[word] = root_counts[word]
-            word_chunks[word] = root_chunks[word]
-    return word_counts, word_chunks
-
     lemmatizer = WordNetLemmatizer()
+    id = wiki_database.title_to_id(title)
+    link_ids = wiki_database.fetch_outgoing_links_set([id])
+    titles = wiki_database.get_titles_set(link_ids)
+    titles = map(lambda title:lemmatizer.lemmatize(extract_title_words(title)[-1]), titles)
+    words = set(titles)
 
-    chunk_counts = dict()
-    chunk_roots = dict()
-    doc = nlp(text)
+    print("Words: {0}".format(len(words)))
 
-    for chunk in doc.noun_chunks:
-        nc = chunk.text.lower()
-        #if ' ' in nc or nc.upper() not in words:
-        #  continue
-        if nc not in chunk_counts:
-            chunk_counts[nc] = 1
-        else:
-            chunk_counts[nc] += 1
-        chunk_roots[nc] = lemmatizer.lemmatize(chunk.root.text)
-
-    bar = progressbar.ProgressBar(maxval=len(words), widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
-    i = 0
-    bar.start()
+    text = page_downloads_database.get_content(title)
+    noun_counts, noun_chunks = page_extractor.extract_noun_chunks(text)
     word_counts = dict()
-    word_chunks = dict()
     for word in words:
-        for chunk in chunk_counts:
-            if word == chunk_roots[chunk].upper():
-                if word not in word_counts or chunk_counts[chunk] < word_counts[word]:
-                    word_counts[word] = chunk_counts[chunk]
-                    word_chunks[word] = chunk
-        i += 1
-        bar.update(i)
-    bar.finish()
-
-    return word_counts, word_chunks
+        if word in noun_counts:
+            word_counts[word] = noun_counts[word]
+    return word_counts
 
 
 def page_ids_to_words(ids):
@@ -80,11 +44,9 @@ def page_ids_to_words(ids):
 
 def get_counts(term):
     noun_counts = {}
-    noun_chunks = {}
     for title in TERM_PAGES[term]:
-        title_noun_counts, title_noun_chunks = get_source_page_counts(title, TimeFromTicks)
+        title_noun_counts = get_source_page_counts(title)
         for noun in title_noun_counts:
             if noun not in noun_counts or noun_counts[noun] < title_noun_counts[noun]:
                 noun_counts[noun] = title_noun_counts[noun]
-                noun_chunks[noun] = title_noun_chunks[noun]
-    return noun_counts, noun_chunks
+    return noun_counts
