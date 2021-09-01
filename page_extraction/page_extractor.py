afd99033ceb208109cd44eb62a71595f9ab1bdb6
Nalu Zou <zonalu@amazon.com>
2021-09-01 09:16:18 -0700

    Optimize page extraction by tokenizing all NEs before counting terms

--- page_extraction/page_extractor.py
+++ page_extraction/page_extractor.py
@@ -3,7 +3,8 @@ import unidecode
 import re
 from nltk import word_tokenize
 from nltk import sent_tokenize
-from nltk import pos_tag
+from nltk.tag.perceptron import PerceptronTagger
+tagger = PerceptronTagger() 
 from nltk.stem import WordNetLemmatizer
 import spacy
 nlp = spacy.load("en_core_web_sm")
@@ -12,6 +13,7 @@ p = inflect.engine()
 
 from utils.nlp_utils import get_ne_chunks
 
+VALID_POS = set(["VB", "NN", "JJ"])
 
 def count_terms(page_title, target_term, text):
     term_counts, excerpts = count_terms_multi(page_title, [target_term], text)
@@ -21,12 +23,13 @@ def count_terms(page_title, target_term, text):
 def count_terms_multi(page_title, target_terms, text):
     sentences = get_sentences(text)
     sentence_nouns_list, sentence_ne_list = get_sentence_nouns(sentences, target_terms)
+    ne_tokenized, ne_counts, ne_sentence_counts = aggregate_ne_list(sentence_ne_list)
 
-    _, _, title_sentence_counts = count_term(page_title, sentences, sentence_nouns_list, sentence_ne_list)
+    _, _, title_sentence_counts = count_term(page_title, sentences, sentence_nouns_list, ne_tokenized, ne_counts, ne_sentence_counts)
     term_counts = {}
     term_excerpts = {}
     for term in target_terms:
-        counted_term, count, sentence_counts = count_term(term, sentences, sentence_nouns_list, sentence_ne_list)
+        counted_term, count, sentence_counts = count_term(term, sentences, sentence_nouns_list, ne_tokenized, ne_counts, ne_sentence_counts)
         term_counts[term] = count
         term_excerpts[term] = get_excerpt(sentence_counts, title_sentence_counts, sentences)
     return term_counts, term_excerpts
@@ -97,7 +100,7 @@ def get_sentence_nouns(sentences, terms):
     terms_set = set(map(lambda term:term.lower(), terms))
 
     for sentence in sentences:
-        pos_words = pos_tag(word_tokenize(sentence))
+        pos_words = tagger.tag(word_tokenize(sentence))
 
         sentence_nouns = filter(lambda word_pos: word_pos[1].startswith('NN') and not word_pos[1] == "NNP", pos_words)
         sentence_nouns = list(map(lambda word_pos: word_pos[0], sentence_nouns))
@@ -118,42 +121,23 @@ def get_sentence_nouns(sentences, terms):
     return sentence_nouns_list, sentence_ne_list
 
 
-def count_term(term, sentences, sentence_nouns_list, sentence_ne_list):
+def count_term(term, sentences, sentence_nouns_list, ne_tokenized, ne_counts, ne_sentence_counts):
     if ' ' in term:
         return count_multi_word_term(term, sentences)
-    return count_single_word_term(term, sentence_nouns_list, sentence_ne_list)
+    return count_single_word_term(term, sentence_nouns_list, ne_tokenized, ne_counts, ne_sentence_counts)
 
 
-def count_single_word_term(term, sentence_nouns_list, sentence_ne_list):
+def count_single_word_term(term, sentence_nouns_list, ne_tokenized, ne_counts, ne_sentence_counts):
     common_noun_count = 0
     common_noun_sentence_counts = []
     variants = set([term, p.plural(term)])
-    print(variants)
     for nouns_list in sentence_nouns_list:
-        print(list(nouns_list))
         count = len(list(filter(lambda noun:noun.upper() in variants, nouns_list)))
-        print(count)
         common_noun_count += count
         common_noun_sentence_counts.append(count)
 
-    ne_counts = {}
-    ne_sentence_counts = []
-    for ne_list in sentence_ne_list:
-        filtered_ne_list = list(filter(lambda ne:term.lower() in (word.lower() for word in word_tokenize(ne)), ne_list))
-        curr_ne_counts = {}
-        for ne in filtered_ne_list:
-            if ne in curr_ne_counts:
-                curr_ne_counts[ne] += 1
-            else:
-                curr_ne_counts[ne] = 1
-
-            if ne in ne_counts:
-                ne_counts[ne] += 1
-            else:
-                ne_counts[ne] = 1
-        ne_sentence_counts.append(curr_ne_counts)
-
-    max_ne = max(ne_counts, key=ne_counts.get) if len(ne_counts) > 0 else None
+    valid_nes = set(filter(lambda ne:term.lower() in ne_tokenized[ne], ne_tokenized.keys()))
+    max_ne = max(valid_nes, key=ne_counts.get) if len(valid_nes) > 0 else None
 
     if max_ne is None or common_noun_count > ne_counts[max_ne]:
         return term.lower(), common_noun_count, common_noun_sentence_counts
@@ -180,3 +164,25 @@ def get_excerpt(term_sentence_counts, title_sentence_counts, sentences):
         if term_sentence_counts[i] > 0 and half_excerpt is None:
             half_excerpt = sentences[i]
     return half_excerpt
+
+
+def aggregate_ne_list(sentence_ne_list):
+    ne_tokenized = {}
+    ne_counts = {}
+    ne_sentence_counts = []
+    for ne_list in sentence_ne_list:
+        curr_ne_counts = {}
+        for ne in ne_list:
+            if ne not in ne_tokenized:
+                ne_tokenized[ne] = set(word.lower() for word in word_tokenize(ne))
+            if ne in curr_ne_counts:
+                curr_ne_counts[ne] += 1
+            else:
+                curr_ne_counts[ne] = 1
+
+            if ne in ne_counts:
+                ne_counts[ne] += 1
+            else:
+                ne_counts[ne] = 1
+        ne_sentence_counts.append(curr_ne_counts)
+    return ne_tokenized, ne_counts, ne_sentence_counts
